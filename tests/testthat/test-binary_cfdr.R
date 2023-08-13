35ec3d5cc0b7f67868a3e8eae1cec0548b9a8474
Tom Willis <tw395@cam.ac.uk>
2023-08-13 19:25:20 +0100

    Added test case for low p-q correlation case to test fix to second if condition

--- tests/testthat/test-binary_cfdr.R
+++ tests/testthat/test-binary_cfdr.R
@@ -13,18 +13,18 @@ test_that("binary_cfdr produces expected result on whole simulated data set", {
   expect_equal(digest::digest(binary_cfdr(p, q, group)), "0a7a84c07f88c3747eaa2f113d882a27")
 })
 
-# NB: would normally be overkill to test this warning condition but I'd like to check my fix of the original error works before I submit a
-test_that("binary_cfdr produces expected warning for low p-q correlation and high proportion of problematic v-values", {
+# NB: would normally be overkill to test this, the evaluation of the if(length(ind) > length(p)*0.5) expression but I'd like to check my fix of the original error works before I submit a PR
+test_that("binary_cfdr executes with low p-q correlation", {
   set.seed(2)
   n <- 10000
   n1p <- 500
   zp <- c(rnorm(n1p, sd=5), rnorm(n-n1p, sd=1))
   p <- 2*pnorm(-abs(zp))
 
-                                        # generate q
+  # generate q
   q <- rbinom(n, 1, 0.1)
 
   group <- c(rep("A", n/2), rep("B", n/2)) 
 
-  expect_warning(binary_cfdr(p, q, group), "p,q have low correlation and >50% of v-values may be problematic - check results")
+  expect_equal(digest::digest(binary_cfdr(p, q, group)), "1ceb99eb0fbcf560d4a340bd7fc67455")
 })
